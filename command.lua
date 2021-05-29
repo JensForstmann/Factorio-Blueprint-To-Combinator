469d13bb1a5fc01825ece933ef74733cf0e1d0ab
Jens Forstmann <19289807+JensForstmann@users.noreply.github.com>
2021-05-29 17:30:16 +0200

    add tiles support to cheat command

--- command.lua
+++ command.lua
@@ -2,18 +2,18 @@
 local EOL = "\n"
 local file_name = "entity-item-map.txt"
 local file_content = ""
-for _, entity_prototype in pairs(game.entity_prototypes) do
-    if entity_prototype.items_to_place_this then
-        local item = entity_prototype.items_to_place_this[1]
+function check_prototype(proto)
+    if proto.items_to_place_this then
+        local item = proto.items_to_place_this[1]
         if type(item) == "string" then
             item = { name = item, count = game.item_prototypes[item].stack_size }
         end
-        if entity_prototype.name ~= item.name or item.count ~= 1 then
-            local section = "\"" .. entity_prototype.name .. "\": {"
-            if entity_prototype.name ~= item.name then
+        if proto.name ~= item.name or item.count ~= 1 then
+            local section = "\"" .. proto.name .. "\": {"
+            if proto.name ~= item.name then
                 section = section .. " item: \"" .. item.name .. "\""
             end
-            if entity_prototype.name ~= item.name and item.count ~= 1 then
+            if proto.name ~= item.name and item.count ~= 1 then
                 section = section .. ","
             end
             if item.count ~= 1 then
@@ -23,5 +23,11 @@ for _, entity_prototype in pairs(game.entity_prototypes) do
             file_content = file_content .. section .. EOL
         end
     end
+end
+for _, entity_prototype in pairs(game.entity_prototypes) do
+    check_prototype(entity_prototype)
+end
+for _, tile_prototype in pairs(game.tile_prototypes) do
+    check_prototype(tile_prototype)
 end
 game.write_file(file_name, file_content)
